7977bc49c4c70bca78bf59272a7bbaf740019dec
kmkzt <info.pscreator@gmail.com>
2020-05-07 11:10:47 +0900

    feat: remove lib file on git

--- lib/useVisible.d.ts
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export declare const useVisible: <T extends Element, V = number>(cb?: ((vi: number) => V) | undefined, option?: Partial<IntersectionObserverInit>) => [React.MutableRefObject<T | null>, V];
